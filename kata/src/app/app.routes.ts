bcdf577bdc807d640d62ede33db8f02d7cebd8ce
abelqasm <abderrahmane.belqasmi@gmail.com>
2024-04-01 13:36:19 +0000

    refactor: remove pages folder

--- kata/src/app/app.routes.ts
+++ kata/src/app/app.routes.ts
@@ -1,15 +1,16 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
 export const routes: Routes = [
   {
     path: '',
     loadComponent: () =>
-      import('./pages/home/home.component').then((c) => c.HomeComponent),
+      import('./products/container/products-list/products-list.component').then(
+        (c) => c.ProductsListComponent
+      ),
   },
   {
     path: 'checkout',
     loadComponent: () =>
-      import('./pages/checkout/checkout.component').then(
+      import('./checkout/component/checkout.component').then(
         (c) => c.CheckoutComponent
       ),
   },
--- kata/src/app/products/container/products-list/products-list.component.ts
+++ kata/src/app/products/container/products-list/products-list.component.ts
@@ -9,11 +9,13 @@ import { ProductCardComponent } from '../../components/product-card/product-card
   selector: 'app-products-list',
   standalone: true,
   imports: [AsyncPipe, ProductCardComponent],
-  template: `<ul class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3">
-    @for (product of (products$ | async); track product.id) {
-    <app-product-card [product]="product" />
-    }
-  </ul>`,
+  template: ` <div class="container max-w-7xl p-10">
+    <ul class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3">
+      @for (product of (products$ | async); track product.id) {
+      <app-product-card [product]="product" />
+      }
+    </ul>
+  </div>`,
 })
 export class ProductsListComponent {
   private readonly productServices = inject(ProductService);
