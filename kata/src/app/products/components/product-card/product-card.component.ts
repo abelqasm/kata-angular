700f74aa5a09013ebb0c11a847e80b73b913fdb0
abelqasm <abderrahmane.belqasmi@gmail.com>
2024-04-01 15:12:12 +0000

    style: adding colors to background/product-card

--- kata/src/app/products/components/product-card/product-card.component.ts
+++ kata/src/app/products/components/product-card/product-card.component.ts
@@ -9,7 +9,9 @@ import { Product } from '../../models/product.model';
   standalone: true,
   imports: [MatCardModule, MatDividerModule],
   template: `
-    <mat-card class="flex gap-2 p-3">
+    <mat-card
+      class="product-card flex gap-2 p-3 cursor-pointer hover:scale-110 hover:z-10 transition-transform duration-200"
+    >
       <mat-card-header class="place-items-start">
         <mat-card-title>{{ product.title }}</mat-card-title>
         <mat-card-subtitle>{{ product.category }}</mat-card-subtitle>
@@ -26,12 +28,13 @@ import { Product } from '../../models/product.model';
       </mat-card-content>
       <mat-divider></mat-divider>
       <mat-card-actions class="w-full">
-        <button mat-button class="w-full bg-red-700 py-2 rounded-lg">
+        <button mat-button class="w-full bg-[#CE5A67] py-2 rounded-lg">
           Add to cart
         </button>
       </mat-card-actions>
     </mat-card>
   `,
+  styles: ['.product-card { background-color: #F4BF96; }'],
 })
 export class ProductCardComponent {
   @Input({ required: true }) product!: Product;
