2356495df0c9a6dcbc4174e32e1b78c8509098a9
abelqasm <abderrahmane.belqasmi@gmail.com>
2024-04-02 13:22:13 +0000

    add: cart servie

--- kata/src/app/checkout/service/cart.service.ts
+++ kata/src/app/checkout/service/cart.service.ts
@@ -1,9 +1,53 @@
 import { Injectable } from '@angular/core';
+import { Product } from 'src/app/products/models/product.model';
+import { BehaviorSubject } from 'rxjs';
+interface CartItem {
+  product: Product;
+  quantity: number;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
+  private cartSubject = new BehaviorSubject<CartItem[]>(this.getCart());
 
-  constructor() { }
+  constructor() {}
+  private getCart(): CartItem[] {
+    const savedCart = localStorage.getItem('cart');
+    return savedCart ? JSON.parse(savedCart) : [];
+  }
+  private saveCart(items: CartItem[]): void {
+    this.cartSubject.next(items);
+    localStorage.setItem('cart', JSON.stringify(items));
+  }
+
+  addTocart(product: Product): void {
+    const items: CartItem[] = this.cartSubject.value;
+    const index = items.findIndex((item) => item.product.id === product.id);
+    if (index !== -1) {
+      items[index].quantity++;
+    } else {
+      items.push({ product, quantity: 1 });
+    }
+    this.saveCart(items);
+  }
+  removeFromCart(productId: number): void {
+    this.saveCart(
+      this.cartSubject.value.filter((item) => item.product.id !== productId)
+    );
+  }
+  quantityHandler(productId: number, quantity: number): void {
+    const items = this.cartSubject.value;
+    const adjustableItem = items.find((item) => item.product.id === productId);
+    if (adjustableItem && quantity > 0) {
+      adjustableItem.quantity = quantity;
+    } else {
+      this.removeFromCart(productId);
+    }
+    this.saveCart(items);
+  }
+  getTotalItem(): number {
+    return this.cartSubject.value.reduce((acc, item) => acc + item.quantity, 0);
+  }
 }
